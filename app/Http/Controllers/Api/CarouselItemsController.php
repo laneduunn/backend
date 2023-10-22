7e053680b305e87d4614767e3359a3d64196acb4
laneduunn <durangoleanne@gmail.com>
2023-10-22 22:54:53 +0800

    Added Delete functionality in CarouselItemsController

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -45,6 +45,10 @@ class CarouselItemsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $CarouselItem = CarouselItems::findOrfail($id);
+ 
+        $CarouselItem->delete();
+
+        return $CarouselItem;
     }
 }
