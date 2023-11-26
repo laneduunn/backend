4e6b53ad7a24c01e64219a9f7f672a5ca37e4058
laneduunn <durangoleanne@gmail.com>
2023-11-26 21:01:05 +0800

    Added Authcontroller and login function with API

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -21,24 +21,26 @@ class UserRequest extends FormRequest
      */
     public function rules(): array
     {
-        if( request()->routeIs('user.store')) {
+        if (request()->routeIs('user.login')) {
+            return [
+                'email'     => 'required|string|email|max:255',
+                'password'  => 'required|min:8',
+            ];
+        } else if (request()->routeIs('user.store')) {
             return [
                 'name'      => 'required|string|max:255',
                 'email'     => 'required|string|email|unique:App\Models\User,email|max:255',
                 'password'  => 'required|min:8',
             ];
-        }
-        else if( request()->routeIs('user.update')) {
+        } else if (request()->routeIs('user.update')) {
             return [
                 'name'      => 'required|string|max:255'
             ];
-        }
-        else if( request()->routeIs('user.email')) {
+        } else if (request()->routeIs('user.email')) {
             return [
                 'email'     => 'required|string|email|max:255'
             ];
-        }
-        else if( request()->routeIs('user.password')) {
+        } else if (request()->routeIs('user.password')) {
             return [
                 'password'  => 'required|confirmed|min:8'
             ];
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CarouselItemsController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,20 +17,28 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('user.login');
+    Route::post('/logout', 'logout')->name('user.logout');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 //carousel
-Route::get('/carousel', [CarouselItemsController::class, 'index']);
-Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
-Route::post('/carousel', [CarouselItemsController::class, 'store']);
-Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
-Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
+Route::controller(CarouselItemsController::class)->group(function () {
+    Route::get('/carousel', 'index');
+    Route::get('/carousel/{id}', 'show');
+    Route::post('/carousel', 'store');
+    Route::put('/carousel/{id}', 'update');
+    Route::delete('/carousel/{id}', 'destroy');
+});
+
 //user
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/{id}', [UserController::class, 'show']);
+// Route::post('/user', [UserController::class, 'store'])->name('user.store');
+// Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+// Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
+// Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
+// Route::delete('/user/{id}', [UserController::class, 'destroy']);
